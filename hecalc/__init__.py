72d7978247b752b717513baadd9f374ee1f80019
Peter E. Martin <76170591+Peter-E-Martin@users.noreply.github.com>
2021-11-10 17:51:49 -0700

    Add versioning info to init file

--- hecalc/__init__.py
+++ hecalc/__init__.py
@@ -2,3 +2,9 @@ from .date_calculation import meesters_dunai, iterated_date, get_date
 from .linear_propagation import date_uncertainty, date_uncertainty_with235
 from .montecarlo import monte_carlo
 from .main import hecalc_main
+
+# Numerical version:
+__version_info__ = (0, 1, 0)
+__version__ = '.'.join(map(str, __version_info__))
+
+__author__ = 'Peter E. Martin <peter.martin-2@colorado.edu>'
