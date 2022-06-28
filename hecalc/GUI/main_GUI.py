710b9b757d4ae562f5084d9376daeee8b15b12b7
Peter E. Martin <76170591+Peter-E-Martin@users.noreply.github.com>
2022-06-28 12:07:17 -0600

    Versioning correction

--- hecalc/GUI/main_GUI.py
+++ hecalc/GUI/main_GUI.py
@@ -111,7 +111,7 @@ class GUI_App(Ui_MainWindow, QObject):
         aboutBut.setWindowTitle("About HeCalc")
         aboutBut.setText(
             '''<html><head/><body><p align=\"center\" style=\"font-size:14px\">
-            v0.4.0 | June 28 2022<br/>Contact: Peter E. Martin
+            v0.4.1 | June 28 2022<br/>Contact: Peter E. Martin
             (peter.martin-2@colorado.edu)<br/>Written in Python 3.8 using
             PyQt5</p></body></html>'''
             )
